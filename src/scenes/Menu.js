9808f636b757f343e97cd550fc9c5a59197123fd
Mia Kennedy <mikkenne@ucsc.edu>
2020-04-08 15:47:31 -0700

    scenes and assets implemented

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,4 +2,11 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
+    create(){
+        //displays menu text
+        this.add.text(20,20, "Rocket Patrol Menu");
+
+        //launches the next scene
+        this.scene.start("playScene");
+    }
 }
